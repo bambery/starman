05370fb5eb4a8f4ab253f7a6426784d895365a1b
bambery <lwszolek@gmail.com>
2013-04-12 19:52:46 -0500

    parsing all file data together to allow for a message on empty post entries

--- post.rb
+++ post.rb
@@ -1,8 +1,12 @@
+require 'date' 
+
 class Post
 
   attr_reader :name, :section, :basename, :metadata, :content
 
   def initialize(post_name)
+
+    raise SystemCallError, "You're not loading the config which contains the env vars." if ENV['POSTS_DIR'].empty?
 
     raise NameError, 'posts must be initialized with a name in the form [section]/[filename w/out ext]' unless /^\w+\/\w+$/ === post_name 
 
@@ -36,7 +40,8 @@ class Post
   def parse_file_data(metadata_text, content)
     # parses date and entry summary, discards any extra metadata
     metadata = Hash.new
-    required_metadata = ["date", "summary"]
+    required_data = ["date", "summary"]
+    required_data << "content" if content.empty?
     metadata_text.lines.each do |mdata_line|
       if is_metadata?(mdata_line)
         #delimit on first colon 
@@ -44,26 +49,30 @@ class Post
         case key.downcase
           when "date"
             # TODO date format localization
+            # TODO custom exception to capture improperly formatted dates and 404 on entry
             metadata["date"] = DateTime.strptime(value, '%m/%d/%Y') 
-            required_metadata.delete("date")
+            required_data.delete("date")
           when "summary"
             metadata["summary"] = value[0..100].strip
-            required_metadata.delete("summary")
+            required_data.delete("summary")
         end #end case
       else p "not valid metadata: #{mdata_line}"
       end #end if
     end # end do
       
     # handle missing required fields
-    if !required_metadata.empty?
-      required_metadata.each do |item|
+    if !required_data.empty?
+      required_data.each do |item|
         case item
           when "date"
             # TODO need a custom exception to handle this so app doesn't blow up on empty entries, should 404 instead
             raise ArgumentError, "Posts must have a date defined on them: #{post_name}"
           when "summary"
             content = "This entry is empty. Please write something here!" if content.empty? 
+            required_data.delete("content") {required_data}
             metadata["summary"] = content[0..100].strip 
+          when "content"
+            content = "This entry is empty. Please write something here!" if content.empty? 
         end # end case
       end # end do
     end # end if
