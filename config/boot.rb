b70b10ad2e95edce35a20c0c4150cc2b18179cb8
bambery <lwszolek@gmail.com>
2013-04-16 12:22:46 -0500

    logging files named by date

--- config/boot.rb
+++ config/boot.rb
@@ -1,10 +1,19 @@
 require 'rubygems'
 require 'bundler'
 require 'sinatra/base'
+require 'date'
+
 Bundler.setup
 
 require File.expand_path('../starman', File.dirname(__FILE__))
+ 
+root_dir = File.expand_path('..', File.dirname(__FILE__))
+
+log = File.new("#{root_dir}/log/starman-#{ENV['RACK_ENV']}-#{Date.today.month}-#{Date.today.day}-#{Date.today.year}.log", "a+")
+$stdout.reopen(log)
+$stderr.reopen(log)
 
 ENV['POSTS_DIR'] = 'content'
+
 
 
