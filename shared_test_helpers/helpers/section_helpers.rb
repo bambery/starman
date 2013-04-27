3a7f1f7fce0443d4f5a0b28d6d83f396e0d9efdc
bambery <lwszolek@gmail.com>
2013-04-26 23:30:23 -0500

    fixing typo in test helper

--- shared_test_helpers/helpers/section_helpers.rb
+++ shared_test_helpers/helpers/section_helpers.rb
@@ -1,4 +1,4 @@
-module SectionHelpers
+module SectionTestHelpers
 
   def create_and_add_section_posts_to_cache(section, trait_names)
     # post_names is an array of FactoryGirl post_data traits.. These posts will be created in the given section and added to the cache. Also assigns a bunch of instance variables for use in tests.
@@ -6,11 +6,11 @@ module SectionHelpers
     trait_names.each do |post_name|
       Post.any_instance.stub(:read_post_file) {FactoryGirl.create(:post_data, post_name.to_sym)}
       instance_variable_set("@#{post_name}", Post.new("#{section}/#{post_name}"))
-      app.settings.memcached.set(post_name, instance_variable_get("@#{post_name}"))
+      app.settings.memcached.set("#{section}/#{post_name}", instance_variable_get("@#{post_name}"))
     end
     return trait_names 
   end
 
 end
 
-World(SectionHelpers) if respond_to?(:World)
+World(SectionTestHelpers) if respond_to?(:World)
