b475ad0b3f49a581ca962b905368a36296d458bd
bambery <lwszolek@gmail.com>
2014-03-19 00:04:55 -0500

    adding cache helper tests

--- spec/caching_helper_spec.rb
+++ spec/caching_helper_spec.rb
@@ -103,7 +103,6 @@ describe Starman do
         c.file("posts/normal/post.mdown", FactoryGirl.create(:post_data))
         CloudCrooner.manifest.compile('normal/post.mdown')
         @post = testapp.new.get_or_add_post_to_cache("normal/post")
-        @get_misses = @memcached.stats[@test_memcached_server]["get_misses"].to_i
         @get_hits= @memcached.stats[@test_memcached_server]["get_hits"].to_i
         @set_count= @memcached.stats[@test_memcached_server]["cmd_set"].to_i
 
@@ -128,10 +127,6 @@ describe Starman do
         @test_memcached_server = '127.0.0.1:11211'
         @memcached = testapp.new.settings.memcached
         @memcached.flush
-
-        @get_misses = @memcached.stats[@test_memcached_server]["get_misses"].to_i
-        @get_hits= @memcached.stats[@test_memcached_server]["get_hits"].to_i
-        @set_count= @memcached.stats[@test_memcached_server]["cmd_set"].to_i
       end
 
       it 'adds a section to the cache' do
@@ -144,22 +139,75 @@ describe Starman do
           Starman::Content.stub(:raw_content_dir).and_return(File.join(c, 'section'))
           Starman::SectionProxy.create_section_proxies
           CloudCrooner.manifest.compile('proxies/blog-proxy.json', 'blog/best_post.mdown', 'blog/second_best.mdown', 'blog/ok_post.mdown')
+
           @section = testapp.new.get_or_add_section_to_cache(section_name)
           expect(@memcached.get(@section.digest_name).posts).to eq(@section.posts)
         end # context
       end # it
-    end # empty cache
-#
-#      it 'adds the section's posts to the cache' do
-#         etc
-#
-#      it 'sorts the section posts by date' do
-#        @section = "blog"
-#        Dir.stub(:entries) {create_and_add_section_posts_to_cache(@section, ["earliest", "most_recent", "middle"])}
-#        @section_posts = cachinghelpers.new.get_or_add_section_to_cache(@section)
-#        expect(@section_posts).to eq(["blog/most_recent", "blog/middle", "blog/earliest"])
-#      end
-#
+
+      it "adds a section's posts to the cache" do
+        within_construct(keep_on_error: true) do |c|
+          CloudCrooner.prefix = 'section'
+          section_name = "blog"
+          c.file("section/blog/best_post.mdown", FactoryGirl.create(:post_data, :best_post))
+          c.file("section/blog/second_best.mdown", FactoryGirl.create(:post_data, :best_post))
+          c.file("section/blog/ok_post.mdown", FactoryGirl.create(:post_data, :ok_post))
+          Starman::Content.stub(:raw_content_dir).and_return(File.join(c, 'section'))
+          Starman::SectionProxy.create_section_proxies
+          CloudCrooner.manifest.compile('proxies/blog-proxy.json', 'blog/best_post.mdown', 'blog/second_best.mdown', 'blog/ok_post.mdown')
+
+          # check to make sure looking for the posts in cache misses
+          # 
+          @get_misses = @memcached.stats[@test_memcached_server]["get_misses"].to_i
+          @get_hits= @memcached.stats[@test_memcached_server]["get_hits"].to_i
+          ['blog/best_post', 'blog/second_best', 'blog/ok_post'].each do |post_name|
+            digest_name = testapp.new.newest_post_digest(post_name)
+            expect(@memcached.get(digest_name)).to eq(nil)
+          end
+
+          expect(@memcached.stats[@test_memcached_server]["get_misses"].to_i).to eq(@get_misses + 3)
+          expect(@memcached.stats[@test_memcached_server]["get_hits"].to_i).to eq(@get_hits)
+
+          @section = testapp.new.get_or_add_section_to_cache(section_name)
+
+          # check to make sure looking for the posts hits
+          # 
+          @get_misses = @memcached.stats[@test_memcached_server]["get_misses"].to_i
+          @get_hits= @memcached.stats[@test_memcached_server]["get_hits"].to_i
+          ['blog/best_post', 'blog/second_best', 'blog/ok_post'].each do |post_name|
+            digest_name = testapp.new.newest_post_digest(post_name)
+            expect(@memcached.get(digest_name)).to be_an_instance_of(Starman::Post) 
+          end
+
+          expect(@memcached.stats[@test_memcached_server]["get_misses"].to_i).to eq(@get_misses)
+          expect(@memcached.stats[@test_memcached_server]["get_hits"].to_i).to eq(@get_hits + 3)
+
+        end # construct 
+      end # it
+
+      it 'sorts the section posts by date' do
+        within_construct do |c|
+          CloudCrooner.prefix = 'section'
+          section_name = "blog"
+          c.file("section/blog/most_recent.mdown", FactoryGirl.create(:post_data, :most_recent))
+          c.file("section/blog/earliest.mdown", FactoryGirl.create(:post_data, :earliest))
+          c.file("section/blog/middle.mdown", FactoryGirl.create(:post_data, :middle))
+          Starman::Content.stub(:raw_content_dir).and_return(File.join(c, 'section'))
+          Starman::SectionProxy.create_section_proxies
+          CloudCrooner.manifest.compile('proxies/blog-proxy.json', 'blog/most_recent.mdown', 'blog/earliest.mdown', 'blog/middle.mdown')
+
+          section = Starman::Section.new("blog", "blog-proxy-fake.json")
+          # posts are unsorted on creation of the section
+          expect(section.posts).to eq([testapp.new.newest_post_digest('blog/earliest'), testapp.new.newest_post_digest('blog/middle'), testapp.new.newest_post_digest('blog/most_recent')])
+
+
+          section = testapp.new.get_or_add_section_to_cache(section_name)
+          expect(section.posts).to eq([testapp.new.newest_post_digest('blog/most_recent'), testapp.new.newest_post_digest('blog/middle'), testapp.new.newest_post_digest('blog/earliest')])
+        end # context
+      end # sorts posts
+
+    end # empty cache tests
+
     # this is a dupe to the first test
 #      it 'creates a new section' do
 #        @section = "blog"
