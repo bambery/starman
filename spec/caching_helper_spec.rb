04889b32cf3de87400d8f5a5ff61da3dacca5a61
bambery <lwszolek@gmail.com>
2014-03-19 11:25:24 -0500

    added cache helper test

--- spec/caching_helper_spec.rb
+++ spec/caching_helper_spec.rb
@@ -134,7 +134,7 @@ describe Starman do
           CloudCrooner.prefix = 'section'
           section_name = "blog"
           c.file("section/blog/best_post.mdown", FactoryGirl.create(:post_data, :best_post))
-          c.file("section/blog/second_best.mdown", FactoryGirl.create(:post_data, :best_post))
+          c.file("section/blog/second_best.mdown", FactoryGirl.create(:post_data, :second_best))
           c.file("section/blog/ok_post.mdown", FactoryGirl.create(:post_data, :ok_post))
           Starman::Content.stub(:raw_content_dir).and_return(File.join(c, 'section'))
           Starman::SectionProxy.create_section_proxies
@@ -150,14 +150,13 @@ describe Starman do
           CloudCrooner.prefix = 'section'
           section_name = "blog"
           c.file("section/blog/best_post.mdown", FactoryGirl.create(:post_data, :best_post))
-          c.file("section/blog/second_best.mdown", FactoryGirl.create(:post_data, :best_post))
+          c.file("section/blog/second_best.mdown", FactoryGirl.create(:post_data, :second_best))
           c.file("section/blog/ok_post.mdown", FactoryGirl.create(:post_data, :ok_post))
           Starman::Content.stub(:raw_content_dir).and_return(File.join(c, 'section'))
           Starman::SectionProxy.create_section_proxies
           CloudCrooner.manifest.compile('proxies/blog-proxy.json', 'blog/best_post.mdown', 'blog/second_best.mdown', 'blog/ok_post.mdown')
 
           # check to make sure looking for the posts in cache misses
-          # 
           @get_misses = @memcached.stats[@test_memcached_server]["get_misses"].to_i
           @get_hits= @memcached.stats[@test_memcached_server]["get_hits"].to_i
           ['blog/best_post', 'blog/second_best', 'blog/ok_post'].each do |post_name|
@@ -171,7 +170,6 @@ describe Starman do
           @section = testapp.new.get_or_add_section_to_cache(section_name)
 
           # check to make sure looking for the posts hits
-          # 
           @get_misses = @memcached.stats[@test_memcached_server]["get_misses"].to_i
           @get_hits= @memcached.stats[@test_memcached_server]["get_hits"].to_i
           ['blog/best_post', 'blog/second_best', 'blog/ok_post'].each do |post_name|
@@ -200,7 +198,7 @@ describe Starman do
           # posts are unsorted on creation of the section
           expect(section.posts).to eq([testapp.new.newest_post_digest('blog/earliest'), testapp.new.newest_post_digest('blog/middle'), testapp.new.newest_post_digest('blog/most_recent')])
 
-
+          # posts are sorted after helper is run
           section = testapp.new.get_or_add_section_to_cache(section_name)
           expect(section.posts).to eq([testapp.new.newest_post_digest('blog/most_recent'), testapp.new.newest_post_digest('blog/middle'), testapp.new.newest_post_digest('blog/earliest')])
         end # context
@@ -208,31 +206,43 @@ describe Starman do
 
     end # empty cache tests
 
-    # this is a dupe to the first test
-#      it 'creates a new section' do
-#        @section = "blog"
-#        Dir.stub(:entries) {create_and_add_section_posts_to_cache(@section, ["best_post", "second_best", "ok_post"])}
-#        @section_double = Section.new(@section)
-#        @section_posts = cachinghelpers.new.get_or_add_section_to_cache(@section)
-#        expect(@section_double.posts - @section_posts).to be_empty 
-#      end
-#    end # empty cache context
-#
-#    context 'the section is in the cache' do
-#      before(:each) do
-#        @test_memcached_server = ENV['TEST_MEMCACHED_SERVER']
-#        app.settings.memcached.flush
-#
-#        @section = "blog"
-#        Dir.stub(:entries) {create_and_add_section_posts_to_cache(@section, ["best_post", "second_best", "ok_post"])}
-#        @section_posts = cachinghelpers.new.get_or_add_section_to_cache(@section)
-#
+    context 'the section is in the cache' do
+      it 'retrieves the section from the cache' do
+        @test_memcached_server = '127.0.0.1:11211'
+        @memcached = testapp.new.settings.memcached
+        @memcached.flush
+        within_construct(keep_on_error: true) do |c|
+          CloudCrooner.prefix = 'section'
+          section_name = "blog"
+          c.file("section/blog/best_post.mdown", FactoryGirl.create(:post_data, :best_post))
+          c.file("section/blog/second_best.mdown", FactoryGirl.create(:post_data, :second_best))
+          c.file("section/blog/ok_post.mdown", FactoryGirl.create(:post_data, :ok_post))
+          Starman::Content.stub(:raw_content_dir).and_return(File.join(c, 'section'))
+          Starman::SectionProxy.create_section_proxies
+          CloudCrooner.manifest.compile('proxies/blog-proxy.json', 'blog/best_post.mdown', 'blog/second_best.mdown', 'blog/ok_post.mdown')
+          
+          section = testapp.new.get_or_add_section_to_cache(section_name)
+
+          @get_misses = @memcached.stats[@test_memcached_server]["get_misses"].to_i
+          @get_hits= @memcached.stats[@test_memcached_server]["get_hits"].to_i
+          @set_count= @memcached.stats[@test_memcached_server]["cmd_set"].to_i
+
+          section2 = testapp.new.get_or_add_section_to_cache(section_name)
+          
+          expect(@memcached.stats[@test_memcached_server]["get_misses"].to_i).to eq(@get_misses)
+          expect(@memcached.stats[@test_memcached_server]["cmd_set"].to_i).to eq(@set_count)
+          expect(@memcached.stats[@test_memcached_server]["get_hits"].to_i).to eq(@get_hits +1)
+
+          expect(section2.posts).to eq(section.posts)
+        end #construct
+      end #it
+    end #context
+
 #        @get_misses = app.settings.memcached.stats[@test_memcached_server]["get_misses"].to_i
 #        @get_hits= app.settings.memcached.stats[@test_memcached_server]["get_hits"].to_i
 #        @set_count= app.settings.memcached.stats[@test_memcached_server]["cmd_set"].to_i
 #
-#      end
-#    
+    
 #      it 'retrieves the section from the cache' do
 #        @blog_posts = cachinghelpers.new.get_or_add_section_to_cache("blog")
 #        @get_misses.should eq(app.settings.memcached.stats[@test_memcached_server]["get_misses"].to_i)
