c76ed670ba1fed3d232aa0e4053af41e79b8a459
bambery <lwszolek@gmail.com>
2014-04-01 17:45:00 -0500

    a more relevant existence check, just for funsies

--- section.rb
+++ section.rb
@@ -11,18 +11,28 @@ module Starman
     end
 
     ##
-    # Returns an unsorted array of the names of the section's posts 
+    # Find all posts in the section, excluding posts that were not compiled. 
+    # Returns an unsorted array of the names of the section's posts.  
     #
     # Can't sort by date here since I don't have access to the cache, and 
     # reading and parsing posts is expensive.
     # 
     def get_posts
+      raise Starman::SectionNotFound.new(section_name) unless Section.exists?(section_name)
       posts = Dir.glob(File.join(Content.raw_content_dir, @name, '/*')) 
       # remove content dir in path to return hash keys
       posts.map! { |post| post.gsub(Content.compiled_content_dir + "/", "") }
-      # remove files without an entry in the manifest 
+      # Remove files without an entry in the manifest. 
       posts.keep_if { |post| Content.newest_post_digest(post) }
     end
 
+    ##
+    # I mean, Jesus, I hope we couldn't get this far with a ghost dir, 
+    # but better safe etc 
+    #
+    def self.exists?(section)
+      return Dir.exists?(File.join(Content.raw_content_dir, section))
+    end
+   
   end
 end
